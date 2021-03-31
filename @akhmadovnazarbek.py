bad9eba0701d21fb18130a8b083c1fdf9d8d2c3a
nazarbek2000 <79694682+nazarbek2000@users.noreply.github.com>
2021-03-31 18:45:45 +0300

    Add files via upload

---
+++ @akhmadovnazarbek.py
@@ -0,0 +1,10 @@
+import cowsay
+cowsay.dragon('Hello World\nMade in Python')
+cowsay.tux('Hello LINUX\nMade in Python')
+
+""" @akhmadovnazarbek Telegram kanal )) """
+
+"""Mana yana qanday rasmlar chiqarsa bo'ladi boshqa metodlar bilan )) 
+['beavis', 'cheese', 'daemon', 'cow', 'dragon', 'ghostbusters', 'kitty', 'meow', 'milk', 'pig', 'stegosaurus', 'stimpy', 'turkey', 'turtle', 'tux']
+
+"""
